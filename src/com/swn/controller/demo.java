21fea5c765f6df633117c80d107adf175976d5d6
shubham <chavhanshubham707@gmail.com>
2024-02-19 23:14:28 +0530

    added first project in git 19th of feb

---
+++ src/com/swn/controller/demo.java
@@ -0,0 +1,31 @@
+package com.swn.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class demo {
+	
+	 public static void main(String[] args) {
+	        int[] numbers = {1, 2, 3, 4, 5, 2, 6, 7, 8, 9, 3};
+
+	        printDuplicateNumbers(numbers);
+	    }
+
+	    static void printDuplicateNumbers(int[] numbers) {
+	        Map<Integer, Integer> numberFrequencyMap = new HashMap();
+
+	        // Count the frequency of each number
+	        for (int num : numbers) {
+	            numberFrequencyMap.put(num, numberFrequencyMap.getOrDefault(num, 0) + 1);
+	        }
+
+	        // Print duplicate numbers
+	        System.out.println("Duplicate numbers:");
+	        for (Map.Entry<Integer, Integer> entry : numberFrequencyMap.entrySet()) {
+	            if (entry.getValue() > 1) {
+	                System.out.println(entry.getKey());
+	            }
+	        }
+	    }
+
+}
